bad83b04d9f6c7cb8937506c4f5a41c168cdf150
Varsha Soni <varsha.soni@poolstack.in>
2024-01-10 12:35:37 +0530

    rails controller

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,35 @@
+class UsersController < ApplicationController
+    def index
+        @user=User.all
+        render json: @user
+
+    end
+
+    def show
+        @user=User.find(params[:id])
+        render json: @user
+    end
+
+    def create 
+        @user=User.new(user_params)
+        render json: @user
+       
+    end
+
+    def update 
+        @user = User.find(params[:id])
+        @user.update(user_params)
+        render json: @user
+    end
+
+    def destroy
+        @user = User.find(params[:id])
+        @user.destroy
+        render json: @user
+    end
+
+    private 
+    def user_params
+        params.require(:user).permit(:name, :email, :city)
+    end
+end
